b6d1065a3c9e9463f17157c7050f6d64fdfd5424
sebaspapu <sebasnarutoxd@gmail.com>
2024-05-19 03:12:55 -0500

    fix: label 'interactuar' in other menus

---
+++ scripts/Interactuar.gd
@@ -0,0 +1,13 @@
+extends Label
+
+var gui = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	gui = get_tree().get_nodes_in_group("gui")[0]
+	gui.get_node("Interactuar").visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
