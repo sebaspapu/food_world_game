0fd4bd4d691914ef8c9cce0fd6eb2d365207cd06
sebaspapu <sebasnarutoxd@gmail.com>
2024-05-27 03:38:58 -0500

    fix: counter ingredients in chatter funcionality
    - se mejoro el mensaje para cuando no hay ingredientes. se ajusto la distribucion de los sprites en el mapa en pro de beneficiar la funcionalidad mencioanda anteriormente

--- scripts/NodeOlla.gd
+++ scripts/NodeOlla.gd
@@ -9,6 +9,7 @@ var gui = null
 var gui2 = null
 var animation_olla = null
 var label_contador6 = null
+var flag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,9 +23,10 @@ func _ready():
 func _input(event):
 	
 	if event.is_action_pressed("com_down"):
+		flag = true
 		label_contador6 = get_tree().get_nodes_in_group("contador_final_ingre")[0].text
 		print("label_contador6: ", label_contador6)
-		if int(label_contador6) >= 2.0:
+		if int(label_contador6) >= 5.0:
 			print("tienes los ingredientes")
 			if isPlayer:
 				var chat = gui.get_node("ChatComprobarIngredientes")
@@ -60,7 +62,9 @@ func _on_giro_timeout():
 func _on_area_2d_body_entered(body):
 	if body.name == "BodyProta":
 		isPlayer = true
-		gui.get_node("Interactuar").visible = true
+		label_contador6 = get_tree().get_nodes_in_group("contador_final_ingre")[0].text
+		if (not flag) or (int(label_contador6) >= 5.0):
+			gui.get_node("Interactuar").visible = true
 
 
 func _on_area_2d_body_exited(body):
